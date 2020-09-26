6da132289c90f9efdc10d3e14763a05b40e4c35f
claylton <clayltonsp@hotmail.com>
2020-09-26 14:47:09 -0300

    Bug teclado resolvido

--- gestro_app/lib/main.dart
+++ gestro_app/lib/main.dart
@@ -4,10 +4,31 @@ import 'pages/cadastro_screen.page.dart';
 import 'pages/login_screen.page.dart';
 
 void main() {
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarBrightness: Brightness.light,
-    statusBarIconBrightness: Brightness.light,
-  ));
-  runApp(CadastroScreen());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.light,
+    ));
+    return MaterialApp(
+      //       localizationsDelegates: [
+      //   GlobalMaterialLocalizations.delegate,
+      //   GlobalWidgetsLocalizations.delegate
+      // ],
+      // supportedLocales: [const Locale('pt', 'BR')],
+      title: 'Gestro App',
+      theme: ThemeData(),
+      debugShowCheckedModeBanner: false,
+      home: CadastroScreen(),
+      // initialRoute: '/',
+      // routes: <String, WidgetBuilder>{
+      //   '/': (context) => CadastroScreen(),
+      // },
+    );
+  }
 }
