93c51bffd116ce6b44043dd2f5b59fe4bf018915
João Vítor <joao.sferreira@souunit.com.br>
2020-10-23 20:30:49 -0300

    exclusão da pasta ios.

--- gestro_app/test_driver/app.dart
+++ gestro_app/test_driver/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:gestro_app/main.dart' as app;
 
+// flutter drive --target=test_driver/app.dart
+
 void main() {
   enableFlutterDriverExtension();
   app.main();
